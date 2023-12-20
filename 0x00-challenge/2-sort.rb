fd81e8b41e3b5938da4116177f57d68c315d2cfb
sebsibe23 <sebsibe1990@proton.me>
2023-12-20 01:05:20 -0800

    q3

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,31 +1,43 @@
 ###
+# Sort integer arguments (ascending)
 #
-#  Sort integer arguments (ascending) 
+# Arguments:
+#   - ARGV: An array of command-line arguments
 #
+# Returns:
+#   - An array of integers in ascending order
 ###
 
-result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
+def sort_arguments(argv)
+  result = []
+  argv.each do |arg|
+    # skip if not an integer
+    next unless arg =~ /^-?[0-9]+$/
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
+      if result[i] < i_arg
+        i += 1
+      else
+        result.insert(i, i_arg)
+        is_inserted = true
+        break
+      end
     end
-    result << i_arg if !is_inserted
+    result << i_arg unless is_inserted
+  end
+
+  result
 end
 
-puts result
+# Sort the arguments passed in from the command line
+sorted_arguments = sort_arguments(ARGV)
+
+# Print the sorted arguments
+puts sorted_arguments
